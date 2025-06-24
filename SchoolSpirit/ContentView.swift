db1883205d24d3e00b04a08e98f63d000a2030db
gallaugher <john.gallaugher@bc.edu>
2025-06-24 11:11:15 -0500

    Initial Commit

---
+++ SchoolSpirit/ContentView.swift
@@ -0,0 +1,22 @@
+//  ContentView.swift
+//  SchoolSpirit
+//  Created by John Gallaugher on 6/24/25.
+//  YouTube.com/profgallaugher - gallaugher.bsky.social
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SchoolSpirit/SchoolSpiritApp.swift
@@ -0,0 +1,15 @@
+//  SchoolSpiritApp.swift
+//  SchoolSpirit
+//  Created by John Gallaugher on 6/24/25.
+//  YouTube.com/profgallaugher - gallaugher.bsky.social
+
+import SwiftUI
+
+@main
+struct SchoolSpiritApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
