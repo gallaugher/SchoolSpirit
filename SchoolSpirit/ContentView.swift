e2dbf54695e0c0b49f4b559c5788a686a6b0a57a
gallaugher <john.gallaugher@bc.edu>
2025-06-24 15:49:45 -0500

    First Commit

--- SchoolSpirit/ContentView.swift
+++ SchoolSpirit/ContentView.swift
@@ -6,12 +6,54 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var message = ""
+    @State private var imageName = ""
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            Spacer()
+            
+            Image(imageName)
+                .resizable()
+                .scaledToFit()
+//                .background(.maroonBC)
+//                .clipShape(RoundedRectangle(cornerRadius: 30))
+                .shadow(radius: 30)
+                .frame(width: 350, height: 350)
+                .animation(.default, value: imageName)
+            
+            Text(message)
+                .font(.largeTitle)
+                .fontWeight(.ultraLight)
+                .animation(.default, value: message)
+            
+            Spacer()
+            
+            HStack {
+                Button {
+                    message = "We Are BC!"
+                    imageName = "eagleLogo"
+                } label: {
+                    // label
+//                    Image(systemName: "graduationcap")
+                    Image("eagleLogo")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: 28)
+                    Text("Who Are We?")
+                }
+                
+                Button("🦅 How is Baldwin?") {
+                    message = "Flyin' High!"
+                    imageName = "baldwin"
+                }
+            }
+            .buttonStyle(.borderedProminent)
+            .tint(.goldBC)
+            .foregroundStyle(.maroonBC)
+            
+
         }
         .padding()
     }
